1527c1abca2d4766f56f9e176df9b4d5822bd2ef
clairemuiru1 <muiruclaire@gmail.com>
2023-11-26 14:52:50 +0300

    "done with Q 2"

---
+++ index.py
@@ -0,0 +1,18 @@
+def exactly_two_positives(a, b, c):
+    # Count the number of positive integers
+    positive_count = 0
+
+    # Check if 'a' is positive
+    if a > 0:
+        positive_count += 1
+
+    # Check if 'b' is positive
+    if b > 0:
+        positive_count += 1
+
+    # Check if 'c' is positive
+    if c > 0:
+        positive_count += 1
+
+    # Return True if exactly two integers are positive, otherwise False
+    return positive_count == 2
